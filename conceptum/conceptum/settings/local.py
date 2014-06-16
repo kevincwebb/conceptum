fb9cb3813623b8dc7ce499fe638d8254d15c233d
Eli Rosenberg <erosenbe@oberlin.edu>
2014-06-16 15:27:26 -0400

    Created Profile Template, added to URLs. Also added setting for AUTH url paths in local settings.

--- conceptum/conceptum/settings/local.py
+++ conceptum/conceptum/settings/local.py
@@ -67,3 +67,15 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
 INTERNAL_IPS = ('127.0.0.1',)
 ########## END TOOLBAR CONFIGURATION
+
+
+########## AUTH CONFIGURATION
+LOGIN_URL = '/accounts/login/'
+
+LOGOUT_URL = '/accounts/logout/'
+
+LOGIN_REDIRECT_URL = '/accounts/profile/'
+
+########## END AUTH CONFIGURATION
+
+
