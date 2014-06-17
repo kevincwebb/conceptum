31fc4361b1ca181c4cb1b9b4e9de9a219b855b7f
Gautam Mohan <s.gautam.mohan@gmail.com>
2014-06-17 16:55:22 -0400

    basic scaffolding to navigate through exams -- mimics polling app

--- conceptum/exam/views.py
+++ conceptum/exam/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from exam.models import Exam
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("exam index!")
+    all_exams = Exam.objects.all()
+    template = loader.get_template('exam/index.html')
+    context = RequestContext(request,
+                             { 'all_exams': all_exams},)
+    return HttpResponse(template.render(context))
+
+def description(request, exam_id):
+    exam_desc = Exam.objects.get(pk=exam_id).description
+    template = loader.get_template('exam/description.html')
+    context = RequestContext(request,
+                             { 'exam_desc': exam_desc,
+                               'exam_id': exam_id},)
+    return HttpResponse(template.render(context))
