4d90c6178ace79da5682ddde46e29a4d631ad345
Gautam Mohan <s.gautam.mohan@gmail.com>
2014-06-17 12:17:22 -0400

    added dummy index page for /exams/ so i could work on comments

--- conceptum/conceptum/urls.py
+++ conceptum/conceptum/urls.py
@@ -19,6 +19,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^exams/', include('exam.urls')),
 )
 
 # Uncomment the next line to serve media files in dev.
