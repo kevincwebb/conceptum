b4fe5eb7c0727b90a6500e914bf7d50c8521b6a0
Kevin Webb <kwebb@cs.swarthmore.edu>
2014-06-19 10:24:16 -0400

    Working on the home page.  Added a generic jumbotron base template that we can extend.

--- conceptum/conceptum/views.py
+++ conceptum/conceptum/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # For CI variable names.
 from django.conf import settings
 
+
 def home(request):
-    context = {}
+    context = {'CI_COURSE': settings.CI_COURSE}
     return render(request, 'home.html', context)
