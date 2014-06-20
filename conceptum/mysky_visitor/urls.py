940c7ead0db423941881d0a04635d8e1f4375e25
erosen14 <erosenbe@oberlin.edu>
2014-06-20 16:17:10 -0400

    implementing mysky

--- conceptum/mysky_visitor/urls.py
+++ conceptum/mysky_visitor/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import *
 from mysky_visitor.views import *
+from sky_visitor.urls import TOKEN_REGEX
 
 urlpatterns = patterns('',
     url(r'^register/$', RegisterView.as_view(), name='register'),
+    url(r'user/invitation/$', CustomInvitationStartView.as_view(), name='invitation_start'),
+    url(r'user/invitation/%s/$' % TOKEN_REGEX, CustomInvitationCompleteView.as_view(), name='invitation_complete'),
     url(r'^', include('sky_visitor.urls')),
     )
--- conceptum/mysky_visitor/views.py
+++ conceptum/mysky_visitor/views.py
@@ -1,8 +1,30 @@
 from sky_visitor import views
 from django.contrib import auth
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+from sky_visitor.views import InvitationStartView
+from sky_visitor.views import InvitationCompleteView
 from sky_visitor.views import RegisterView as BaseRegisterView
 from sky_visitor.forms import RegisterForm
+
+
 
 class RegisterView(BaseRegisterView):
     template_name = 'mysky_visitor/registration_form.html'
    
+
+class CustomInvitationStartView(InvitationStartView):
+    template_name = 'exampleapp/invitation_start.html'
+    success_message = _("Invitation successfully delivered. Invite more friends!")
+ 
+    def get_email_kwargs(self, user):
+        kwargs = super(CustomInvitationStartView, self).get_email_kwargs(user)
+        # Custom subject line
+        kwargs['subject'] = "Your friend invited you to join the community!"
+        return kwargs
+    
+class CustomInvitationCompleteView(InvitationCompleteView):
+    template_name = 'exampleapp/invitation_complete.html'
+ 
+    def get_success_url(self):
+        return reverse('home')
