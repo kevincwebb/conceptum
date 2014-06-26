81b07716ad00dc2a554bdd2f443e5e297eb92e33
erosen14 <erosenbe@oberlin.edu>
2014-06-26 11:55:24 -0400

    Made default user setting isActive = false, for new user approval

--- conceptum/users/models.py
+++
@@ -1,7 +0,0 @@
-from authtools.models import User as BaseUser
-from simple_email_confirmation import SimpleEmailConfirmationUserMixin
- 
-    
-class User(SimpleEmailConfirmationUserMixin, BaseUser):
-    class Meta:
-        proxy = True
