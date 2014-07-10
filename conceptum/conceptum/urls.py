77b33c1acfcd8b340aa6b63c613f166abfcc060f
Gautam Mohan <s.gautam.mohan@gmail.com>
2014-07-10 11:43:14 -0400

    started working on stage1 page hierarchy. landing tree prints bullets with no text.

--- conceptum/conceptum/urls.py
+++ conceptum/conceptum/urls.py
@@ -24,7 +24,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^exams/', include('exam.urls')),
-    
+    url(r'^stage1/', include('cistage1.urls')),
 
 )
 
