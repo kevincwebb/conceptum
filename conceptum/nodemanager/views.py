bad842044cb94cf1426a7a4d0d8e9d92f98906ff
Gautam Mohan <s.gautam.mohan@gmail.com>
2014-07-17 14:28:45 -0400

    can now add multiple atoms at once using formsets

--- conceptum/nodemanager/views.py
+++ conceptum/nodemanager/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
+from django.forms.formsets import formset_factory
 
 from nodemanager.models import CITreeInfo, ConceptNode, ConceptAtom
-from nodemanager.forms import AtomForm
+from nodemanager.forms import AtomForm, AtomFormSet
 # Create your views here.
 
 getNode = lambda node_id: ConceptNode.objects.filter(pk=node_id).get()
@@ -13,13 +14,19 @@ getNode = lambda node_id: ConceptNode.objects.filter(pk=node_id).get()
 def entry(request, node_id, redirected=False):
 
     node = getNode(node_id)
-    form = AtomForm();
+    user = request.user
+    atoms = ConceptAtom.objects.filter(user=user)
+
+    form_params = {
+        'extra' : 5,
+    }
+    formset = formset_factory(AtomForm, **form_params)
 
     template = loader.get_template('nodemanager/entry.html')
     context = RequestContext(request,
                              {'node': node,
-                              'user': request.user,
-                              'form': form,
+                              'user': user,
+                              'formset': formset,
                               'redirected': redirected},)
     return HttpResponse(template.render(context))
 
@@ -28,19 +35,27 @@ def entry(request, node_id, redirected=False):
 # incorrectly entered
 def get_entry(request, node_id):
     if request.method == 'POST':
-        form = AtomForm(request.POST)
-        if form.is_valid():
-            new_atom = ConceptAtom(
-                concept_node=getNode(node_id),
-                user=request.user,
-                text=form.cleaned_data['text'],
-                final_choice=False
-            )
-            new_atom.save()
+        formset = AtomFormSet(request.POST)
+        if formset.is_valid():
+
+            for form in formset:
+                print form.cleaned_data.get('text')#['text']
+
+                text = form.cleaned_data.get('text')
+
+                if text: #only non-empty forms have text
+                    new_atom = ConceptAtom(
+                        concept_node=getNode(node_id),
+                        user=request.user,
+                        text=form.cleaned_data['text'],
+                        final_choice=False
+                    )
+                    new_atom.save()
+
             return redirect('redirected free entry',
                             node_id=node_id, redirected=True)
         else:
-            form = AtomForm() #form wasn't valid so we make a new one
+            form = AtomFormSet(AtomForm, extra=5) #return new form
 
     return render(request, 'nodemanager/entry.html',
                   {'node': getNode(node_id),
