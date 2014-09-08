00f282ee10a25b15e3d03520d310ee7da341b922
Gautam Mohan <s.gautam.mohan@gmail.com>
2014-09-08 17:14:09 -0400

    added value counter initialization to get_setup + other stuff

--- conceptum/ranking/views.py
+++ conceptum/ranking/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 
 from nodemanager.models import ConceptNode
-from ranking.models import RankingProcess
+from ranking.models import RankingProcess, ValueCounter
 from ranking.forms import RankingProcessSetupForm, BinaryChoiceForm
 
 # TODO: get_or_404s for all nodes, ranking processes, etc
@@ -49,11 +49,21 @@ def get_setup(request, node_id):
         form = RankingProcessSetupForm(request.POST)
         if form.is_valid():
 
+            #update ranking_process state based on admin form submission
             ranking_process = RankingProcess.objects.filter(parent__pk=node_id).get()
             ranking_process.choices = node.get_final_choices()
             ranking_process.value_choices = form.cleaned_data.get('value_choices')
             ranking_process.status = ranking_process.in_progress
             ranking_process.save()
+
+            #add corresponding value counter to each choice
+            for choice in ranking_process.choices.all():
+                value_counter = ValueCounter(target = choice,
+                                             ranking_process = ranking_process,
+                                             value = 0)
+                value_counter.save()
+
+            
 
             return redirect('dispatch', node_id=node_id)
             #return HttpResponse("valid form!...db code tba, {0}".format(form.cleaned_data.get('value_choices')))
@@ -73,13 +83,12 @@ def submit(request, node_id):
     
     if user not in node.users_contributed_set():
 
+        render_args = {'node': node,
+                       'user': user,
+                       'form': form,}
+        
         if user in node.admin_set():
-            return render(request, 'ranking/rank_submit_admin.html',
-                          {'node': node,
-                           'user': user,
-                           'form': form}
-                      )
-
+            return render(request, 'ranking/rank_submit_admin.html', render_args)
         else:
             return render(request, 'ranking/rank_submit_user.html',)
 
@@ -95,6 +104,24 @@ def get_submit(request, node_id):
     user = request.user
     node = ConceptNode.objects.filter(pk=node_id).get()
 
+    if request.method == 'POST':
+
+        form = BinaryChoiceForm(request.POST, node_id=node_id)
+
+        if form.is_valid():
+
+            choices = form.cleaned_data['final_choices']
+
+            # for choice in choices:
+            #     counter = ValueCounter.objects.filter(target=atom).get()
+            #     counter.value += 1
+            #     counter.save()
+                                
+            print choices
+            return HttpResponse("yo it's valid")
+
+        else:
+            return HttpResponse("uh oh not valid")    
     
     #return redirect('final sub', node_id=node_id)
 
