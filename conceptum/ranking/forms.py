2dd23b20db7ebc541f0fccb8f4023bc0102ee33f
Gautam Mohan <s.gautam.mohan@gmail.com>
2014-09-28 19:33:19 -0400

    added code to solve the problem of not being able to pass a python list to a queryset field

--- conceptum/ranking/forms.py
+++ conceptum/ranking/forms.py
@@ -1,6 +1,22 @@
 from django import forms
+from django.contrib.contenttypes.models import ContentType
+
 from ranking.models import RankingProcess
-from nodemanager.models import ConceptAtom
+from nodemanager.models import ConceptNode, ConceptAtom
+
+### For some reason imports weren't working so this re-declaration is
+### a crufty workaround
+
+#given a model, return its ContenType (needed for generic keys
+#declared in ranking app models)
+def get_ct(model):
+    return ContentType.objects.get_for_model(model)
+
+#given a node, get the ranking process attached to it (via generic query)
+def get_ranking_process(node):
+    return RankingProcess.objects.filter(object_id=node.id,
+                                         content_type=get_ct(node)).get()
+
 
 # this may seem superfluous now, but in the future if/when we support
 # more exotic kinds of ranking, as well as voting deadlines, it will
@@ -19,6 +35,18 @@ class BinaryChoiceForm(forms.Form):
     def __init__(self, *args, **kwargs):
         
         node_id = kwargs.pop('node_id')
-        ranking_process = RankingProcess.objects.filter(parent__id=node_id).get()
+        node = ConceptNode.objects.filter(pk=node_id).get()
+        ranking_process = get_ranking_process(node)
         super(BinaryChoiceForm, self).__init__(*args, **kwargs)
-        self.fields['final_choices'].queryset = ranking_process.choices.all()
+
+        # This is needed because there's no way we can return a
+        # QUERYSET of things from get_rank_choices without losing
+        # generality. We right now have a list of the things, and we
+        # need to convert it to a queryset, since this is the only
+        # type (to my knowledge) that the 'queryset' field of a form
+        # field will accept. Thus, we get each atom's id and search
+        # for them all again, except this time they are returned as a
+        # queryset.
+        atom_ids = map(lambda obj: obj.id, ranking_process.get_rank_choices())
+        atom_qset = ConceptAtom.objects.filter(pk__in=atom_ids)
+        self.fields['final_choices'].queryset = atom_qset
