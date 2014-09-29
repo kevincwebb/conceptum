1838796068a77d1d0bdca49cd7815ed8ff6a0faf
Gautam Mohan <s.gautam.mohan@gmail.com>
2014-09-29 19:34:07 -0400

    submit form somewhat functional...doesn't check for errors

--- conceptum/cistage1/urls.py
+++ conceptum/cistage1/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
             url(r'^$', views.dispatch, name='stage1 dispatch'),
             url(r'^setup', views.setup, name='stage1 setup'),
             url(r'^getsetup$', views.get_setup, name = 'stage 1 get setup'),
-            url(r'^$', views.landing, name='landing'),
+            url(r'^landing$', views.landing, name='landing'),
             url(r'^node/', include('nodemanager.urls')),
         )
