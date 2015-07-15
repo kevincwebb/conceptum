72b6e4fcb70ac296aa50b7660ad948db311f820f
Ben Rempel <benrempel93@gmail.com>
2015-07-15 12:42:29 -0400

    added InterviewGroups to Interviews

--- conceptum/interviews/admin.py
+++ conceptum/interviews/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Interview, Excerpt
+from .models import Interview, Excerpt, InterviewGroup
+
+
+class InterviewGroupAdmin(admin.ModelAdmin):
+	list_display = ('name', 'unlocked')
 
 
 class InterviewAdmin(admin.ModelAdmin):
@@ -11,5 +15,6 @@ class ExcerptAdmin(admin.ModelAdmin):
 	list_display = ('content_object', 'interview', 'response', 'object_id')
 
 
+admin.site.register(InterviewGroup, InterviewGroupAdmin)
 admin.site.register(Interview, InterviewAdmin)
 admin.site.register(Excerpt, ExcerptAdmin)
--- conceptum/interviews/urls.py
+++ conceptum/interviews/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-    #/interviews/
     url(r'^$', views.IndexView.as_view(), name='interview_index'),
-    #/interviews/add/
-    url(r'^add/$', views.AddView.as_view(), name='interview_add'),
-    #/interviews/1/
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='interview_detail'),
-    #/interviews/1/edit/
+    url(r'^create/$', views.CreateGroupView.as_view(), name='interview_create'),
+    url(r'^(?P<group_id>\d+)/$', views.GroupView.as_view(), name='interview_group'),
+    url(r'^(?P<group_id>\d+)/rename/$', views.RenameView.as_view(), name='interview_rename'),
+    url(r'^(?P<group_id>\d+)/lock/$', views.lock_group, name='interview_lock'),
+    url(r'^(?P<group_id>\d+)/unlock/$', views.unlock_group, name='interview_unlock'),
+    url(r'^(?P<group_id>\d+)/add/$', views.AddView.as_view(), name='interview_add'),
+    url(r'^(?P<pk>\d+)/detail/$', views.DetailView.as_view(), name='interview_detail'),
     url(r'^(?P<pk>\d+)/edit/$', views.EditView.as_view(), name='interview_edit'),
-    #/interviews/1/delete/
     url(r'^(?P<pk>\d+)/delete/$', views.DeleteView.as_view(), name='interview_delete'),
 )
